8751e6268ddf1b643a47caeea28b2269d5e9a789
Michael Starkman <michaelstarkman@Michaels-MacBook-Pro-2.local>
2022-04-11 18:57:15 -0600

    updated model

--- tickets_api/models.py
+++ tickets_api/models.py
@@ -1,10 +1,14 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 class Ticket(models.Model):
     name = models.CharField(max_length=32)
     venue = models.CharField(max_length=32) 
-    # consider adding min_length=2 
-    # date = models.DateTimeField
+    date = models.DateTimeField(default='2022-01-01')
     price = models.IntegerField()
-    #    img(Str)
+    image = models.CharField(default='https://i.imgur.com/A79Xirs.jpeg', max_length=100)
+    # image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100,)
+
+
+
