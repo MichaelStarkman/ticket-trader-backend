3adc6244a435ed943621abf81bea2ff3810fb507
Michael Starkman <michaelstarkman@Michaels-MacBook-Pro-2.local>
2022-04-11 19:12:59 -0600

    model updated again

--- tickets_api/models.py
+++ tickets_api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Ticket(models.Model):
     name = models.CharField(max_length=32)
     venue = models.CharField(max_length=32) 
-    date = models.DateTimeField(default='2022-01-01')
+    date = models.DateField(default='2022-01-01')
     price = models.IntegerField()
     image = models.CharField(default='https://i.imgur.com/A79Xirs.jpeg', max_length=100)
     # image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100,)
