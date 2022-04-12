42de9a1f8c0ec74ccf7d098b61ca380fdbff2a06
Michael Starkman <michaelstarkman@Michaels-MacBook-Pro-2.local>
2022-04-11 19:38:38 -0600

    changed date to event date

--- tickets_api/models.py
+++ tickets_api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Ticket(models.Model):
     name = models.CharField(max_length=32)
     venue = models.CharField(max_length=32) 
-    # date = models.DateField(null=True)
+    event_date = models.DateField(null=True)
     price = models.IntegerField()
     # image = models.CharField(max_length=100)
     # image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100,)
