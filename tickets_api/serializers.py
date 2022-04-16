4a16a26dc8bea28a31cd96f616f9f2e4692852dc
Michael Starkman <michaelstarkman@Michaels-MacBook-Pro-2.local>
2022-04-15 18:13:30 -0600

    create modal up

--- tickets_api/serializers.py
+++ tickets_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Ticket
 class TicketSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Ticket # tell django which model to use
-        fields = ('id', 'name', 'venue', 'event_date', 'price', 'img') # tell django which fields to include
+        fields = ('id', 'name', 'venue', 'event_date', 'price') # tell django which fields to include
